9fda3cd2c60d267e9cc0ef09c60a4b721a033c73
elfuska <38552542+elfuska@users.noreply.github.com>
2023-02-24 20:04:17 +0100

    Update fabglconf.h

--- esp32/TinyCPCEMttgovga32/CPCem/fabglconf.h
+++ esp32/TinyCPCEMttgovga32/CPCem/fabglconf.h
@@ -38,5 +38,9 @@
 //Defines ESP32 XTAL frequency in Hz.
 #define FABGLIB_XTAL 40000000
 
+/** Core to use for CPU intensive tasks like VGA signals generation in VGATextController or VGAXXController */
+#define FABGLIB_VIDEO_CPUINTENSIVE_TASKS_CORE (WIFI_TASK_CORE_ID ^ 1) // using the same core of WiFi may cause flickering
+
+
 #endif
 #endif
