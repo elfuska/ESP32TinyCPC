ed39d8a1bea3788ff9b2fdd228a426f8843334f1
elfuska <38552542+elfuska@users.noreply.github.com>
2023-03-03 19:47:08 +0100

    Update FDC.h

--- esp32/TinyCPCEMttgovga32/CPCem/FDC.h
+++ esp32/TinyCPCEMttgovga32/CPCem/FDC.h
@@ -2,6 +2,7 @@
  #define _FDC_H
  
  void getDskList(void);
+ void mountSD(void);
  void loaddsk2Flash(unsigned char id);
  unsigned char readfdc(unsigned short addr);
  void writefdc(unsigned short addr, unsigned char val);
